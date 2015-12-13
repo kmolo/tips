7e528ecdbb5fce0a9c4904913df18a1a76317e9e
Katherine Moloney <kmoloney@mail.ccsf.edu>
2015-12-13 02:00:09 -0800

    add SegmentedController + Stepper to SettingViewController

--- tips/SettingsViewController.swift
+++ tips/SettingsViewController.swift
@@ -10,17 +10,63 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    
+    @IBOutlet weak var setTipControl: UISegmentedControl!
+    @IBOutlet weak var setTipStepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        
+        let currentTipPercentage = setTipControl.titleForSegmentAtIndex(setTipControl.selectedSegmentIndex)!
+        let endIndex = currentTipPercentage.endIndex.advancedBy(-1)
+        
+        setTipStepper.value = Double(currentTipPercentage.substringToIndex(endIndex))!
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onSetTipControlChanged() {
+        let currentTipPercentage = setTipControl.titleForSegmentAtIndex(setTipControl.selectedSegmentIndex)!
+        let endIndex = currentTipPercentage.endIndex.advancedBy(-1)
+        
+        setTipStepper.value = Double(currentTipPercentage.substringToIndex(endIndex))!
+        
+    }
+    @IBAction func onStepperChanged(sender: UIStepper) {
+        
+        let newTipPercentage = String(Int(sender.value)) + "%"
+        let currentIndex = setTipControl.selectedSegmentIndex
+        
+        setTipControl.setTitle(newTipPercentage, forSegmentAtIndex: currentIndex)
+        
+        
+    }
 
     /*
     // MARK: - Navigation
